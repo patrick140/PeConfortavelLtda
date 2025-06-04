ff4f84c06a90c834fbae6a7ee724bd22cbe9236a
patrick140 <96891037+patrick140@users.noreply.github.com>
2025-06-03 23:00:15 -0300

    Commit final do dia 03-06-25

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Clientes(models.model):
+    cpf = models.CharField(primary_key=True,max_length=11, 
+                           help_text='Cpf do cliente')
+    nome = models.CharField(max_length=70, 
+                            help_text='Nome do cliente')
+    endereco = models.CharField(max_length=100,
+                                help_text='Endereço do Cliente')
+    telefone = models.CharField(max_length=11,
+                                help_text='Telefone do cliente')
+    estadoDeDomicilio = models.CharField(max_length=2,
+                                help_text='Estado de Domicilío do cliente')
+    cidade = models.CharField(max_length=50,
+                                help_text='Cidade do cliente')
+    genero = models.CharField(max_length=1,
+                                help_text='Genero do cliente')
+    contato = models.CharField(max_length=1,
+                                help_text='Contato do cliente')
+    emailDoCliente = models.CharField(max_length=100,
+                                help_text='E-mail do cliente')
+    nomeDoUsuario = models.CharField(max_length=100,
+                                help_text='Nome do usuario')
+    senha = models.CharField(max_length=256,
+                                help_text='Senha')
+
+
