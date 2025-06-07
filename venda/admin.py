b79baed6a2f17ef6bf2deebac67e6f629f5a43d9
patrick140 <96891037+patrick140@users.noreply.github.com>
2025-06-06 21:32:40 -0300

    commit final do dia 06-06-25

--- venda/admin.py
+++ venda/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from venda.models import Venda
 
 # Register your models here.
+admin.site.register(Venda)
--- venda/models.py
+++ venda/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Venda(models.Model):
+    numero = models.AutoField(primary_key=True, 
+                           help_text='Numero da venda')
+    data_venda = models.DateField(null=True,
+                                  blank=True,
+                                  default=timezone.now(), 
+                            help_text='data da venda')
+    valor_venda = models.FloatField(help_text='Valor da venda')
+    
+    cliente_cpf = models.CharField(max_length=11, 
+                                   unique=True)
+    
+    def __str__(self):
+        return f'{self.numero} - {self.cliente_cpf}'
